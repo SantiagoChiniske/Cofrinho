e76ab84b54e67202263109ea961a50f56a8c46e9
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-07-14 20:29:51 -0300

    Uso teclado e switch

--- cofrinho/src/uninter/Menu.java
+++ cofrinho/src/uninter/Menu.java
@@ -1,6 +1,14 @@
 package uninter;
 
+import java.util.Scanner;
+
 public class Menu {
+	
+	private Scanner sc;
+	
+	public Menu() {
+		sc = new Scanner(System.in);
+	}
 	
 	public void exibirMenuPrincipal() {
 		System.out.println("COFRINHO:");
@@ -9,6 +17,21 @@ public class Menu {
 		System.out.println("3-Lista moedas:");
 		System.out.println("4-Calcular valor total convertido para real:");
 		System.out.println("0-Encerrar");
+		
+		String opcao = sc.next();
+		
+		switch (opcao) {
+		
+		
+		case"0": 
+			System.out.println("Encerrando...");
+			break;
+			
+		default:
+			System.out.println("Opção inválida!");
+			exibirMenuPrincipal();
+			break;
+		}
 	}
 	
 
