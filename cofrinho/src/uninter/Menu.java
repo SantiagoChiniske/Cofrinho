559375d362fe7d8c6d88b1524954589b15f8c55d
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-07-14 20:20:28 -0300

    Exibir o menu

---
+++ cofrinho/src/uninter/Menu.java
@@ -0,0 +1,15 @@
+package uninter;
+
+public class Menu {
+	
+	public void exibirMenuPrincipal() {
+		System.out.println("COFRINHO:");
+		System.out.println("1-Adicionar moeda:");
+		System.out.println("2-Remover moeda");
+		System.out.println("3-Lista moedas:");
+		System.out.println("4-Calcular valor total convertido para real:");
+		System.out.println("0-Encerrar");
+	}
+	
+
+}
